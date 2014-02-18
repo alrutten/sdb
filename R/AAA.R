bc835f0548ec5305e72280a44e090ea2b1f1f61c
Mihai Valcu <valcu@orn.mpg.de>
2014-02-18 22:09:08 +0100

    -

--- R/AAA.R
+++ R/AAA.R
@@ -1,7 +1,9 @@
 
 
 .onLoad <- function(libname, pkgname){
-    packageStartupMessage(paste('This is', pkgname, dcf[, "Version"] ))
+  dcf <- read.dcf(file=system.file("DESCRIPTION", package=pkgname) )
+  packageStartupMessage(paste('This is', pkgname, dcf[, "Version"] ))
+  
   if( ! Sys.info()["sysname"] %in% c("Linux", "Windows")) 
     packageStartupMessage("sdb might not work under", OS)
 }
