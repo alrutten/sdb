51eceeee986de26a3689e1275abdb43c769310e5
ValcuMihai <valcu.mihai@gmail.com>
2014-03-08 16:23:21 +0100

    sqlFormat() a wrapper to sqlparse https://github.com/andialbrecht/sqlparse

--- R/sqlSnippets.R
+++ R/sqlSnippets.R
@@ -1,12 +1,20 @@
 
-
-# https://github.com/andialbrecht/sqlparse/
-# ???? pip install sqlparse
-
 sqlFormat <-function(x ){
-  require(rPython)
   
+  # https://github.com/andialbrecht/sqlparse/
   
+  if( require(rPython)  & !inherits(try(python.exec("import sqlparse"), silent = TRUE), "try-error") ){  
+x = gsub("\n", " ", x)
+python.exec("
+def pyf( sql ):
+  'sqlparse.format wrapper '
+  return sqlparse.format(sql, reindent=True, keyword_case='upper')
+  ")
+  x = python.call ("pyf", x)
+  } 
+return(x)
+
+
 }
 
 
@@ -15,7 +23,8 @@ snipSave <- function(query, description='', host = "scidb.mpio.orn.mpg.de") {
   if(!credentialsExist(host)) stop("Pls. save your credentials first.\nRun: saveCredentials(user = '', password = '', database = '') " )
 	eval(parse(text = readLines(credentialsPath(host))[-1]))	
 
-
+  query = sqlFormat(query)
+  
 	v = paste("INSERT INTO DBLOG.snippets (query, author, description) VALUES(",
 			paste(shQuote(query),shQuote(user), shQuote(description), sep = ",")   ,");")
 
@@ -28,4 +37,5 @@ snipSave <- function(query, description='', host = "scidb.mpio.orn.mpg.de") {
 	
 
 }
+
 
