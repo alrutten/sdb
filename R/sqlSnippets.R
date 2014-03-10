7e70017fd47cac6861a2bd949237016b7f8bcb06
ValcuMihai <valcu.mihai@gmail.com>
2014-03-10 11:00:08 +0100

    -

--- R/sqlSnippets.R
+++ R/sqlSnippets.R
@@ -3,27 +3,27 @@ sqlFormat <-function(x ){
   
   # https://github.com/andialbrecht/sqlparse/
   
-  if( require(rPython)  & !inherits(try(python.exec("import sqlparse"), silent = TRUE), "try-error") ){  
-x = gsub("\n", " ", x)
-python.exec("
-def pyf( sql ):
-  'sqlparse.format wrapper '
-  return sqlparse.format(sql, reindent=True, keyword_case='upper')
-  ")
-  x = python.call ("pyf", x)
-  } 
-return(x)
+	  if( require(rPython)  & !inherits(try(python.exec("import sqlparse"), silent = TRUE), "try-error") ){  
+	x = gsub("\n", " ", x)
+	python.exec("
+	def pyf( sql ):
+	  'sqlparse.format wrapper '
+	  return sqlparse.format(sql, reindent=True, keyword_case='upper')
+	  ")
+	  x = python.call ("pyf", x)
+	  } 
+	return(x)
 
 
 }
 
 
-snipSave <- function(query, description='', host = "scidb.mpio.orn.mpg.de") {
+snipSave <- function(query, description='', format = TRUE, host = "scidb.mpio.orn.mpg.de") {
 
   if(!credentialsExist(host)) stop("Pls. save your credentials first.\nRun: saveCredentials(user = '', password = '', database = '') " )
 	eval(parse(text = readLines(credentialsPath(host))[-1]))	
 
-  query = sqlFormat(query)
+  if(format) query = sqlFormat(query)
   
 	v = paste("INSERT INTO DBLOG.snippets (query, author, description) VALUES(",
 			paste(shQuote(query),shQuote(user), shQuote(description), sep = ",")   ,");")
@@ -38,4 +38,37 @@ snipSave <- function(query, description='', host = "scidb.mpio.orn.mpg.de") {
 
 }
 
+snipLoadFromFile <-function(f) {
+	
+	x = paste(scan(f, what = 'character',multi.line = FALSE), collapse = " ")
+	x = strsplit(x, ";", fixed = TRUE)[[1]]
+	
+	for(i in 1:length(x)) snipSave(x[[i]])
+	
+	
+	
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
