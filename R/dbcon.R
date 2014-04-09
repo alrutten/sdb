2cf5a6d79ccc26896900dc38c389562a86c4c198
alrutten <anne.rutten@gmail.com>
2014-04-09 11:54:17 +0200

    typo

--- R/dbcon.R
+++ R/dbcon.R
@@ -1,4 +1,3 @@
-
 dbcon <- function(user, password, database, host = "scidb.mpio.orn.mpg.de", path) {
 
   # figure out credentials
@@ -22,7 +21,7 @@ dbcon <- function(user, password, database, host = "scidb.mpio.orn.mpg.de", path
   
   if(OS == "Windows") {
     require(RODBC)
-    conStr=paste0("SERVER=",host,";DRIVER=MySQL ODBC 5.2 Unicode Driver;UID=",user,";PWD=",password,";case=nochange;functions return string=yes"")
+    conStr=paste0("SERVER=",host,";DRIVER=MySQL ODBC 5.2 Unicode Driver;UID=",user,";PWD=",password,";case=nochange;functions return string=yes")
     con = odbcDriverConnect(connection=conStr) 
     if (con==-1) stop('please download and install the ODBC connector from https://dev.mysql.com/downloads/connector/odbc/')
     if( !missing(database) )
